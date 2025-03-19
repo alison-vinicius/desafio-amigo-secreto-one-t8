a1d3defe84db55176a6a837746bacf75c3c46e82
alison-vinicius <alison.soares@sou.ufac.br>
2025-03-18 20:59:36 -0500

    versão final

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = []
+
+function adicionarAmigo(){
+    let amigo = document.querySelector('input').value
+    let lista = document.getElementsByClassName('listaDeAmigos')[0]
+    let paragrafo = document.createElement('p')
+
+    if (amigo.trim() !== ""){
+        amigos.push(amigo)
+        paragrafo.textContent = amigo
+        lista.appendChild(paragrafo)
+        document.querySelector('input').value = ""
+    } else{
+        alert("insira um nome válido")
+    }
+
+    
+    console.log(amigos)
+}
+
+
+function sortearAmigo(){
+    let indice = Math.floor(Math.random() * amigos.length);
+    let lista = document.getElementsByClassName('listaDeAmigos')[0]
+    let mensagemAmigoSecreto = `<p style="color: green; font-weight: bold;">o amigo secreto sorteado é ${amigos[indice]}`
+    lista.innerHTML = mensagemAmigoSecreto
+
+    amigos = []
+}
+
+
